5b3949864154f7335d12aa953466c8819baf9c05
lindwurm <lindwurm.q@gmail.com>
2017-09-05 05:59:25 +0900

    Raise the limit of profiles.
    * username: 30 -> 32
    * display_name: 30 -> 32
    * note: 500 -> 512
    
    Signed-off-by: lindwurm <lindwurm.q@gmail.com>

--- app/models/account.rb
+++ app/models/account.rb
@@ -76,11 +76,11 @@ class Account < ApplicationRecord
   validates :username, format: { with: /\A#{USERNAME_RE}\z/i }, if: -> { !local? && will_save_change_to_username? }
 
   # Local user validations
-  validates :username, format: { with: /\A[a-z0-9_]+\z/i }, length: { maximum: 30 }, if: -> { local? && will_save_change_to_username? && actor_type != 'Application' }
+  validates :username, format: { with: /\A[a-z0-9_]+\z/i }, length: { maximum: 32 }, if: -> { local? && will_save_change_to_username? && actor_type != 'Application' }
   validates_with UniqueUsernameValidator, if: -> { local? && will_save_change_to_username? }
   validates_with UnreservedUsernameValidator, if: -> { local? && will_save_change_to_username? }
-  validates :display_name, length: { maximum: 30 }, if: -> { local? && will_save_change_to_display_name? }
-  validates :note, note_length: { maximum: 500 }, if: -> { local? && will_save_change_to_note? }
+  validates :display_name, length: { maximum: 32 }, if: -> { local? && will_save_change_to_display_name? }
+  validates :note, note_length: { maximum: 512 }, if: -> { local? && will_save_change_to_note? }
   validates :fields, length: { maximum: 4 }, if: -> { local? && will_save_change_to_fields? }
 
   scope :remote, -> { where.not(domain: nil) }
--- spec/models/account_spec.rb
+++ spec/models/account_spec.rb
@@ -606,20 +606,20 @@ RSpec.describe Account, type: :model do
         expect(account).to model_have_error_on_field(:username)
       end
 
-      it 'is invalid if the username is longer then 30 characters' do
-        account = Fabricate.build(:account, username: Faker::Lorem.characters(number: 31))
+      it 'is invalid if the username is longer then 32 characters' do
+        account = Fabricate.build(:account, username: Faker::Lorem.characters(number: 33))
         account.valid?
         expect(account).to model_have_error_on_field(:username)
       end
 
-      it 'is invalid if the display name is longer than 30 characters' do
-        account = Fabricate.build(:account, display_name: Faker::Lorem.characters(number: 31))
+      it 'is invalid if the display name is longer than 32 characters' do
+        account = Fabricate.build(:account, display_name: Faker::Lorem.characters(number: 33))
         account.valid?
         expect(account).to model_have_error_on_field(:display_name)
       end
 
-      it 'is invalid if the note is longer than 500 characters' do
-        account = Fabricate.build(:account, note: Faker::Lorem.characters(number: 501))
+      it 'is invalid if the note is longer than 512 characters' do
+        account = Fabricate.build(:account, note: Faker::Lorem.characters(number: 513))
         account.valid?
         expect(account).to model_have_error_on_field(:note)
       end
@@ -652,20 +652,20 @@ RSpec.describe Account, type: :model do
         expect(account).to model_have_error_on_field(:username)
       end
 
-      it 'is valid even if the username is longer then 30 characters' do
-        account = Fabricate.build(:account, domain: 'domain', username: Faker::Lorem.characters(number: 31))
+      it 'is valid even if the username is longer then 32 characters' do
+        account = Fabricate.build(:account, domain: 'domain', username: Faker::Lorem.characters(number: 33))
         account.valid?
         expect(account).not_to model_have_error_on_field(:username)
       end
 
-      it 'is valid even if the display name is longer than 30 characters' do
-        account = Fabricate.build(:account, domain: 'domain', display_name: Faker::Lorem.characters(number: 31))
+      it 'is valid even if the display name is longer than 32 characters' do
+        account = Fabricate.build(:account, domain: 'domain', display_name: Faker::Lorem.characters(number: 33))
         account.valid?
         expect(account).not_to model_have_error_on_field(:display_name)
       end
 
-      it 'is valid even if the note is longer than 500 characters' do
-        account = Fabricate.build(:account, domain: 'domain', note: Faker::Lorem.characters(number: 501))
+      it 'is valid even if the note is longer than 512 characters' do
+        account = Fabricate.build(:account, domain: 'domain', note: Faker::Lorem.characters(number: 513))
         account.valid?
         expect(account).not_to model_have_error_on_field(:note)
       end
