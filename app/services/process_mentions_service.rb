6b8b18b0c792f39b36ca44b6b514abe925bf5a55
umonaca <myunknownemail@protonmail.com>
2019-08-26 03:44:05 -0400

    Revert "Do not leak local-only toots to remote mentioned users"
    This reverts commit 1d471a9d03b3860f2a9cec94433ccc1a146ddd1b.

--- app/services/process_mentions_service.rb
+++ app/services/process_mentions_service.rb
@@ -48,7 +48,7 @@ class ProcessMentionsService < BaseService
 
     if mentioned_account.local?
       LocalNotificationWorker.perform_async(mentioned_account.id, mention.id, mention.class.name)
-    elsif mentioned_account.activitypub? && !@status.local_only?
+    elsif mentioned_account.activitypub?
       ActivityPub::DeliveryWorker.perform_async(activitypub_json, mention.status.account_id, mentioned_account.inbox_url)
     end
   end
