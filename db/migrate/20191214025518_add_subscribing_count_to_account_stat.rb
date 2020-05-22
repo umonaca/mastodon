d9bf29615804140ac3bd7332e60dd338fd31f205
umonaca <myunknownemail@protonmail.com>
2020-05-22 02:56:24 -0400

    Fix strong migrations

--- db/migrate/20191214025518_add_subscribing_count_to_account_stat.rb
+++ db/migrate/20191214025518_add_subscribing_count_to_account_stat.rb
@@ -1,5 +1,7 @@
 class AddSubscribingCountToAccountStat < ActiveRecord::Migration[5.2]
   def change
-    add_column :account_stats, :subscribing_count, :bigint, null: false, default: 0
+    safety_assured do
+      add_column :account_stats, :subscribing_count, :bigint, null: false, default: 0
+    end
   end
 end
