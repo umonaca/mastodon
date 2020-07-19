4ed56af8b18f4955d32e314ef5f70bf9d3d4d467
noellabo <noel.yoshiba@gmail.com>
2020-07-20 05:39:49 +0900

    Fix visibility compatibility

--- app/serializers/rest/status_serializer.rb
+++ app/serializers/rest/status_serializer.rb
@@ -4,7 +4,7 @@ class REST::StatusSerializer < ActiveModel::Serializer
   attributes :id, :created_at, :in_reply_to_id, :in_reply_to_account_id,
              :sensitive, :spoiler_text, :visibility, :language,
              :uri, :url, :replies_count, :reblogs_count,
-             :favourites_count, :local_only
+             :favourites_count, :local_only, :limited
 
   attribute :favourited, if: :current_user?
   attribute :reblogged, if: :current_user?
@@ -62,6 +62,10 @@ class REST::StatusSerializer < ActiveModel::Serializer
     else
       object.visibility
     end
+  end
+
+  def limited
+    object.limited_visibility?
   end
 
   def uri
