a398fb125804c698dfc6ccd263b9fcd27a192663
umonaca <myunknownemail@protonmail.com>
2020-10-29 00:30:32 -0400

    Add account sensitized
    Fix i18n
    
    Fix spec

--- config/routes.rb
+++ config/routes.rb
@@ -242,6 +242,7 @@ Rails.application.routes.draw do
     resources :accounts, only: [:index, :show, :destroy] do
       member do
         post :enable
+        post :unsensitive
         post :unsilence
         post :unsuspend
         post :redownload
@@ -491,6 +492,7 @@ Rails.application.routes.draw do
         resources :accounts, only: [:index, :show, :destroy] do
           member do
             post :enable
+            post :unsensitive
             post :unsilence
             post :unsuspend
             post :approve
