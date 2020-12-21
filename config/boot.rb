f3b03dbe9ebd23fc0fb8c528813cd42ead5977cd
umonaca <myunknownemail@protonmail.com>
2020-12-21 07:30:25 -0500

    Revert "Add clean error message when RAILS_ENV is unset (#15381)"
    This reverts commit 9734c9b6fe2d2e4a980d8ad94c5de0300b23c809.

--- config/boot.rb
+++ config/boot.rb
@@ -1,8 +1,3 @@
-unless ENV.key?('RAILS_ENV')
-  STDERR.puts 'ERROR: Missing RAILS_ENV environment variable, please set it to "production", "development", or "test".'
-  exit 1
-end
-
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../Gemfile', __dir__)
 
 require 'bundler/setup' # Set up gems listed in the Gemfile.
